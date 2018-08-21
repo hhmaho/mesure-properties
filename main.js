ad55b73aafbf6bb857cde530107b5442764f3267
Intec <Intec@Computer_02.cursisten.intecbrussel.intra>
2018-08-21 14:14:24 +0200

    google sheet linken in js en html file met vue en tabletop

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 function init() {
   Tabletop.init({
-    key: 'https://docs.google.com/spreadsheets/d/0AmYzu_s7QHsmdDNZUzRlYldnWTZCLXdrMXlYQzVxSFE/pubhtml',
-    callback: function (data, tabletop) {
-      console.log(data)
+    key: 'https://docs.google.com/spreadsheets/d/1HQLmOx7gHBh1SvKWuBSQB5xt1JD-hTwxsAtXUpprmiU/edit?usp=sharing',
+    // ignore this URL: https://docs.google.com/spreadsheets/d/e/2PACX-1vTqti2RKa4EpXpJBkSKuoSyHlFTbjFJouk8qOmk7klhdSkJlwa08EpYfBGE4GQKT4FDt1wOS2Dr8AUh/pubhtml
+    callback: function (tabletopData, tabletop) {
+      console.log(tabletopData)
+
+      var app4 = new Vue({
+        el: '#app-4',
+        data: {
+          list: tabletopData,
+        }
+      })
     },
     simpleSheet: true
   })
 }
+
 window.addEventListener('DOMContentLoaded', init)
