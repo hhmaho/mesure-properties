ed2006b0458035ffa9c4642e703199695a076b0d
hhmaho <hhmaho@gmail.com>
2018-08-23 15:53:55 +0200

    functie aanpassing js om de VUE te verbergen

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 function init() {
+  var elem = document.getElementById('app-4')
+  elem.style.display = 'none'
+
   Tabletop.init({
     key: 'https://docs.google.com/spreadsheets/d/1HQLmOx7gHBh1SvKWuBSQB5xt1JD-hTwxsAtXUpprmiU/pubhtml',
     callback: function (tabletopData, tabletop) {
       console.log(tabletopData)
+      elem.style.display = 'block'
 
       var app4 = new Vue({
         el: '#app-4',
