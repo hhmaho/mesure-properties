a05b5484fdc6248ba75e7e7c28f6cade47cc3ec9
hhmaho <hhmaho@gmail.com>
2018-08-24 12:07:50 +0200

    aanpassing test.js

--- test.js
+++ test.js
@@ -1,13 +1,10 @@
-for (var i = 0; i < 101; i = i + 1) { console.log (i);
-
+for (var i = 1; i < 101; i = i + 1) { console.log(i)
     if (i % 15 == 0){
-        console.log("fizzbuzz") 
-    } 
-    else if (i % 5 == 0){
-        console.log("buzz")
-    }
-    else (i % 3 == 0){
-        console.log("fizz")
+    console.log("fizzbuzz"); 
+    }   else if (i % 5 == 0){
+        console.log("buzz");
+    }   else (i % 3 == 0){
+        console.log("fizz");
     }
 }
    
