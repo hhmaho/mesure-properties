9201761e7a39d776cec1b04ea62b8e5b21bf3ce7
hhmaho <hhmaho@gmail.com>
2018-08-24 11:58:28 +0200

    oefening javascript

---
+++ test.js
@@ -0,0 +1,14 @@
+for (var i = 0; i < 101; i = i + 1) { console.log (i);
+
+    if (i % 15 == 0){
+        console.log("fizzbuzz") 
+    } 
+    else if (i % 5 == 0){
+        console.log("buzz")
+    }
+    else (i % 3 == 0){
+        console.log("fizz")
+    }
+}
+   
+    
