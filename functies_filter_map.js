e356b7da793a32f5dd14a90811c49b53009dfa02
Vassili Moltchanov <vassilimoltchanov@iMacvanVassili.lan>
2018-08-29 11:32:16 +0200

    bestanden verplaatsen

--- functies_filter_map.js
+++
@@ -1,8 +0,0 @@
-function veelverdienders (workers, limiet) {
-    //filter & map
-    return workers
-    .filter(function (worker) { return worker.salary >= limiet })
-    .map(function (worker) { return worker.name})
-}
-
-console.log(veelverdienders(workers, 2000))
