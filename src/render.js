51ca9b78b924a610d2be19a6dfcc3613f14362ab
hhmaho <hhmaho@gmail.com>
2018-09-11 13:37:53 +0200

    opkuisen

--- src/render.js
+++ src/render.js
@@ -1,9 +1,10 @@
 var crel = require('crel')
 
 function renderHeader() {
- return crel('h1', 'mh')
-// return crel('div', 
- //crel('img', {src: '/img/logo.png'})
+ return crel('div', {'id':'wrap'},
+ crel('img', {src: '/img/logo.png'}),
+ crel('h1', 'index'),
+ )
 }
 
 function renderFooter() {
@@ -11,8 +12,17 @@ function renderFooter() {
 }
 //entry vervangen met unit
 function renderUnit(unit) {
-    return crel('li', unit.naam, unit.afkorting)
+    return crel('li',
+     crel('span', {'class':'naam'}, unit.naam), 
+     crel('span', {'class':'afkorting'}, unit.afkorting),
+     crel('span', {'class':'wikilink'},
+      crel ('a', {'href':unit.wikilink},unit.wikilink)),
+     crel('span', {'class':'wow'}, unit.wieofwat),
+     crel('span', {'class':'foto'}, 
+      crel ('img', {'src': unit.foto})),
+    )
    }
+
 //test
 function renderUnits(units) {
   return crel('ul',
